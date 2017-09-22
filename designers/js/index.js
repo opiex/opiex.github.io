3b47c1516b568dba2174f991de0258be62b27144
Ariel Verber <opiedesigns@gmail.com>
2017-09-22 13:17:30 +0300

    changed colors, added 2 designers, changed * to emoji

--- designers/js/index.js
+++ designers/js/index.js
@@ -1,18 +1,30 @@
 /*jshint esversion: 6 */
 
-
-var hexas = ['7200FF', '0062FF', 'FF3992', 'DEA300', '00FFE4', '8A1E6E', 'E43162', '1E2F8A'];
+var hexas = [
+  '7200FF',
+  '0062FF',
+  'FF3992',
+  'DEA300',
+  '00FFE4',
+  '8A1E6E',
+  'E43162',
+  '1E2F8A'
+];
 var currentHexa = 0;
 var showEmployed = 1;
 
+var delay = 0;
+
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
+  var j,
+    x,
+    i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
 }
 
 var item = `
@@ -43,36 +55,35 @@ var item = `
   </div>
 `;
 
-function moveInsideLeft(){
+function moveInsideLeft() {
   $('.inside').removeClass('insideright').addClass('insideleft');
 }
 
-function moveInsideRight(){
+function moveInsideRight() {
   $('.inside').removeClass('insideleft').addClass('insideright');
 }
 
-
-function getRandomGradient(){
-  return 'linear-gradient(224deg, ' +getnextHexa() +' 0%, rgba(9,17,28,0.00) 100%)';
+function getRandomGradient() {
+  return 'linear-gradient(224deg, ' + getnextHexa() + ' 0%, rgba(9,17,28,0.00) 100%)';
 }
 
-function getnextHexa(){
-  if(currentHexa === hexas.length - 1){
+function getnextHexa() {
+  if (currentHexa === hexas.length - 1) {
     currentHexa = 0;
-    return '#'+hexas[hexas.length - 1];
-  }
-  else{
+    return '#' + hexas[hexas.length - 1];
+  } else {
     currentHexa++;
-    return '#'+hexas[currentHexa - 1];
+    return '#' + hexas[currentHexa - 1];
   }
 }
 
-function generateGradient(){
+function generateGradient() {
   return "#7200FF";
 }
 
-function createNewDiv(image, name, website, companies, status, email){
+function createNewDiv(image, name, website, companies, status, email) {
   var div = document.createElement('div');
+  var a = document.createElement('a');
 
   div.className = "item";
   div.innerHTML = item;
@@ -89,194 +100,161 @@ function createNewDiv(image, name, website, companies, status, email){
   newName.append(name);
 
   //Website
+  a.href = "http://" +website;
+  $(a).addClass("linkOfItem");
+  a.target = "_blank";
   newWebsite.append(website);
-  newWebsite.href="http://"+website;
+  // newWebsite.href = newWebsite;
 
   //Companies
   newCompanies.append(companies);
 
   //Current Status
-  if(status === 'employed'){
+  if (status === 'employed') {
     newStatus.append("Happily Employed 💼");
-  }
-  else if(status === 'freelance'){
+  } else if (status === 'freelance') {
     newStatus.append("Freelance 🖼");
-  }
-  else{
+  } else {
     newStatus.append("Looking for Opportunities ☺️");
   }
 
   //Email
   newContact.append(email);
-  newContact.href="mailto:" +email;
+  newContact.href = "mailto:" + email;
 
   //Done
-  $('.items').append(div);
+  $(a).append(div);
+  $('.items').append(a);
 
   //Background image + gradient
-  newBackground =  getRandomGradient()+", url('img/designers/" +image +"') no-repeat center";
+  newBackground = getRandomGradient() + ", url('img/designers/" + image + "') no-repeat center";
   $(newOverlay).css("background", newBackground);
 }
 
 // createNewDiv("0002.gif", "Ariel Verber", "www.arielverber.com", "YO", "employed", "hi@arielverber.com");
 // createNewDiv("0001.gif", "Artiom Dashinsky", "www.arielverber.com", "YO", "freelance", "hi@arielverber.com");
 
-
 var list = [
-	{
-		img: "tevihirshhorn.jpg",
+  {
+    img: "tevihirshhorn.jpg",
     name: "Tevi Hirschhorn",
     website: "www.tevster.com",
     companies: "-",
     current: "employed",
     contact: "tevi@tevster.com"
-	},
-
-  {
-		img: "liatabadi.jpg",
+  }, {
+    img: "liatabadi.jpg",
     name: "Liat Abadi",
     website: "abadiguard.com",
     companies: "Better Place, JPEGmini, Unomy",
     current: "employed",
     contact: "liat@abadiguard.com"
-	},
-
-  {
-		img: "hilapeleg.gif",
+  }, {
+    img: "hilapeleg.gif",
     name: "Hila Peleg",
     website: "www.behance.net/hilapeleg",
     companies: "Inkod Hypera ltd, Slidely, Playbuzz",
     current: "employed",
     contact: "hilapeleg1984@gmail.com "
-	},
-
-  {
-		img: "eyalzuri.png",
+  }, {
+    img: "eyalzuri.png",
     name: "Eyal Zuri",
     website: "dribbble.com/eyalz",
     companies: "Muzli, InVision, Netcraft",
     current: "employed",
     contact: "zurieyal@gmail.com"
-	},
-
-  {
-		img: "robinkane.gif",
+  }, {
+    img: "robinkane.gif",
     name: "Robin Kane",
     website: "www.robinkane-art-house.com/",
     companies: "WIX, Horizon 2020, European Union",
     current: "employed",
     contact: "robinkane.art@gmail.com"
-	},
-
-  {
-		img: "edenvidal.png",
+  }, {
+    img: "edenvidal.png",
     name: "Eden Vidal",
     website: "edenvidal.com",
     companies: "YOTPO, Alooma, Zebra Medical",
     current: "freelance",
     contact: "edenvidal@gmail.com"
-	},
-
-  {
-		img: "lironcohen.png",
+  }, {
+    img: "lironcohen.png",
     name: "Liron Cohen",
     website: "lironic-design.com",
     companies: "BlueVine. Armis. Culture trip",
     current: "freelance",
     contact: "lironicdesign@gmail.com"
-	},
-
-  {
-		img: "adammorad.png",
+  }, {
+    img: "adammorad.png",
     name: "Adam Morad",
     website: "dribbble.com/Adammorad",
     companies: "Netvertise, Yotpo, Wisdo",
     current: "employed",
     contact: "Adammorad7@gmail.com"
-	},
-
-  {
-		img: "roihiman.png",
+  }, {
+    img: "roihiman.png",
     name: "Roi Himan",
     website: "roi-himan.com",
     companies: "Wix",
     current: "employed",
     contact: "roihim@gmail.com"
-	},
-
-  {
-		img: "izikrachman.png",
+  }, {
+    img: "izikrachman.png",
     name: "Izik Rachman",
     website: "www.izikrachman.com",
     companies: "Fiverr, Intel",
     current: "freelance",
     contact: "izik.rachman@gmail.com"
-	},
-
-  {
-		img: "nathanfreifeld.png",
+  }, {
+    img: "nathanfreifeld.png",
     name: "Nathan Freifeld",
     website: "www.nathanfreifeld.com/",
     companies: "Mobilize, Spot.IM, Saatchi&Saatchi",
     current: "employed",
     contact: "nfreifeld@gmail.com"
-	},
-
-  {
-		img: "limorpeleg.png",
+  }, {
+    img: "limorpeleg.png",
     name: "Limor Peleg",
     website: "www.behance.net/peleglimor0aeb",
     companies: "Pionet Group, JFrog, Kaminario, Discount Bank",
     current: "freelance",
     contact: "peleg.limor@gmail.com"
-	},
-
-  {
-		img: "alextrue.jpg",
+  }, {
+    img: "alextrue.jpg",
     name: "Alex True",
     website: "alex-true.com",
     companies: "Publicis Digital, Moburst and Freelance",
     current: "unemployed",
     contact: "trublenkov@gmail.com"
-	},
-
-  {
-		img: "omerfrank.png",
+  }, {
+    img: "omerfrank.png",
     name: "Omer Frank",
     website: "mrfrank.me",
     companies: "Waves, Polyverse",
     current: "freelance",
     contact: "omer@frankscreative.com"
-	},
-
-  {
-		img: "eladnaider.png",
+  }, {
+    img: "eladnaider.png",
     name: "Elad Naider",
     website: "dribbble.com/eladnaider",
     companies: "HP, SaferVPN",
     current: "employed",
     contact: "eladnaider@gmail.com"
-	},
-
-  {
-		img: "omerassa.gif",
+  }, {
+    img: "omerassa.gif",
     name: "Omer Assa",
     website: "dribbble.com/Omerico",
     companies: "Mreshet, Zeek, torii",
     current: "employed",
     contact: "omer@jointorii.co"
-	},
-
-  {
+  }, {
     img: "artiomdashinsky.gif",
     name: "Artiom Dashinsky",
     website: "www.dashinsky.com",
     companies: "WeWork, InVision, SodaStream",
     current: "freelance",
     contact: "hvoostik@gmail.com"
-  },
-
-  {
+  }, {
     img: "galshir.gif",
     name: "Gal Shir",
     website: "www.galshir.com",
@@ -300,18 +278,35 @@ var list = [
     website: "daskulll.tumblr.com",
     companies: "YOTPO, eBay, Purple",
     current: "employed",
-    contact: "-"
+    contact: "valentinburtakov@gmail.com"
+  },
+
+  {
+    img: "marinasokolov.png",
+    name: "Marina Sokolov",
+    website: "www.marinzart.com",
+    companies: "Juno, eToro, Spot.IM",
+    current: "employed",
+    contact: "marinz.box@gmail.com"
+  },
+
+  {
+    img: "urizur.jpg",
+    name: "Uri Zur",
+    website: "www.urizur.com",
+    companies: "Sears, Waze, Zlango",
+    current: "employed",
+    contact: "uriandzur@gmail.com"
   }
 ];
 
-function toggleEmployed(){
-  if(showEmployed === 0){
+function toggleEmployed() {
+  if (showEmployed === 0) {
     showEmployed = 1;
     $('.border').removeClass("faded");
     $('.toggle').removeClass("faded");
     moveInsideRight();
-  }
-  else{
+  } else {
     showEmployed = 0;
     $('.border').addClass("faded");
     $('.toggle').addClass("faded");
@@ -322,25 +317,30 @@ function toggleEmployed(){
 
 shuffle(list);
 
-function loadElements(){
+function loadElements() {
+  delay = 0;
+  var j = 0;
   document.getElementsByClassName('items')[0].innerHTML = "";
-  $('.numberofdesigners').html(list.length);
   var current = "";
   console.log(showEmployed);
-  if(showEmployed !== 0){
-  for(i = 0; i <= list.length - 1; i++){
-    current = list[i];
-    createNewDiv(current.img, current.name, current.website, current.companies, current.current, current.contact);
-  }
-  }
-  else{
-    for(i = 0; i <= list.length - 1; i++){
+  if (showEmployed !== 0) {
+    for (i = 0; i <= list.length - 1; i++) {
       current = list[i];
-      if(current.current !== "employed"){
       createNewDiv(current.img, current.name, current.website, current.companies, current.current, current.contact);
+      $('.numberofdesigners').html(i + 1);
+    }
+  } else {
+    for (i = 0; i <= list.length - 1; i++) {
+      current = list[i];
+      if (current.current !== "employed") {
+        createNewDiv(current.img, current.name, current.website, current.companies, current.current, current.contact);
+        j++;
+        $('.numberofdesigners').html(j);
       }
     }
   }
 }
 
 loadElements();
+
+// $('.item').click()
